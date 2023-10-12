df350b279a933f320ec5948eeb7f02175deb050c
h_swathi_shenoy <hswathishenoy@gmail.com>
2023-10-12 22:27:27 +0530

    ci/cd for load stage

---
+++ src/load.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sys
+import os
+from omegaconf import DictConfig, OmegaConf
+from utils.pathfinder import PathConfig
+from sklearn.model_selection import train_test_split
+
+path = PathConfig()
+configs_dir = path.configs_dir
+data_dir = path.data_dir
+
+params = OmegaConf.load(configs_dir.joinpath('params.yaml'))['load']
+input_file = sys.argv[1]
+os.makedirs(os.path.join(data_dir, 'prepared'), exist_ok=True)
+
+
+def load_data() -> None:
+    data = pd.read_csv(data_dir.joinpath(input_file))
+    X = data.iloc[:, :-1].values
+    y = data.iloc[:, -1].values
+    x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=params['random_state'],
+                                                        test_size=params['split'])
+    train_df = pd.concat([pd.DataFrame(x_train), pd.DataFrame(y_train)], axis=1)
+    test_df = pd.concat([pd.DataFrame(x_test), pd.DataFrame(y_test)], axis=1)
+    train_df.to_csv(data_dir.joinpath('prepared/train.csv'))
+    test_df.to_csv(data_dir.joinpath('prepared/test.csv'))
+    return
+
+
+if __name__=='__main__':
+    load_data()
---
+++ src/utils/pathfinder.py
@@ -0,0 +1,26 @@
+import typing as t
+from pathlib import Path
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class PathConfig:
+    base_path: t.Optional[Path] = Path(__file__).absolute().parent.parent.parent
+    data_dir: t.Optional[Path] = None
+    configs_dir: t.Optional[Path] = None
+
+    def __post_init__(self):
+        self.data_dir = self.base_path.joinpath('data')
+        self.configs_dir = self.base_path.joinpath('configs')
+
+    def to_dict(self):
+        path = asdict(self)
+        paths = {k: str(v) for k, v in path.items()}
+        return paths
+
+
+if __name__ == '__main__':
+    path_repo = PathConfig()
+    print(path_repo.to_dict())
+
+
